d3f5e379778852b429f8db567ef78fee34942bcf
mkogw <mkogw2016@icloud.com>
2023-04-24 21:34:30 +0900

    task01 finished

--- task01/main.cpp
+++ task01/main.cpp
@@ -40,9 +40,13 @@ Eigen::Vector2f time_integration_implicit(const Eigen::Vector2f& p0, float dt){
   Eigen::Matrix2f A;
   Eigen::Vector2f b;
   // modify the following two lines to implement implicit time integration
-  A << 1.f, 0.f, 0.f, 1.f;
-  b << r0, v0;
-  return A.inverse()*b;
+  //A << 1.f, 0.f, 0.f, 1.f;
+  //b << r0, v0;
+  A << 1.f/dt, -1.f, -dfdr, 1.f/dt;
+  //A << 1.f/dt,  1.f/dt-dfdr, 1.f/dt-1.f, 1.f/dt;
+  b << v0, f0;
+  //return A.inverse()*b;
+  return p0 + A.inverse()*b;
 }
 
 /**
